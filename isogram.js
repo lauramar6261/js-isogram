e8da14ad46fafd6c7863dffa9a87d77307fa45d9
Ada <larmartinez2@gmail.com>
2018-11-17 12:47:15 -0800

    full answer

--- isogram.js
+++ isogram.js
@@ -1,5 +1,23 @@
 const isIsogram = function isIsogram(text) {
-  return true;
+  let answer;
+  if ((text === '') || (text.length === 1)) {
+    answer = true;
+  }
+  const letters = {};
+  // remove hyphen of space before looping for text
+
+  text.replace('-', '').replace(' ', '').toUpperCase().split('')
+    .forEach((letter) => {
+      letters[letter] = 0;
+    });
+  if (Object.keys(letters).length === text.replace(' ', '').replace('-', '').length) {
+    answer = true;
+  } else {
+    answer = false;
+  }
+  // console.log(letters);
+  // console.log(text);
+  return answer;
 };
 
 export default isIsogram;
